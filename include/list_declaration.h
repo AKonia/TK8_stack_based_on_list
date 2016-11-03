2e9d8f8aa2f6d5ea3b4d5a1d71b0ca816b316165
Alexandr Konakov <konakov@my.com>
2016-11-03 23:42:43 +0300

    Вроде доделал написал 3 теста

---
+++ include/list_declaration.h
@@ -0,0 +1,31 @@
+#ifndef LIST_DECLARATION_HPP
+#define LIST_DECLARATION_HPP
+
+template<typename ListDataType>
+class List
+{
+protected:
+    struct Node
+    {
+        ListDataType value;
+        Node * next;
+    };
+    Node * currentNode;
+    unsigned int listSize;
+public:
+    List();
+    List(const List & rhs);
+    ~List();
+
+    ListDataType & operator[](unsigned int indx);
+    List & operator=(const List & rhs);
+
+    unsigned int getSize();
+    bool isEmpty();
+
+    void push(const ListDataType value, int indx = -1);
+    void removeElem(unsigned int indx);
+
+
+};
+#endif // LIST_DECLARATION_HPP
---
+++ samples/core.cpp
@@ -0,0 +1,14 @@
+#include "Stack.h"
+#include <iostream>
+int main()
+{
+	Stack<int> stk;
+	for(int i = 0; i < 15; i++)
+		stk.push(i);
+	while(!stk.isEmpty())
+	{	
+		std::cout << stk.top() << " ";
+		stk.pop();	
+	}
+	return 0;
+}
